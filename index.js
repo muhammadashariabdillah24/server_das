7ddc3715cde5656b19fc62888a5f52c038bde136
muhammadashariabdillah24 <tutordunet09@gmail.com>
2022-02-14 13:41:55 +0700

    Server Untuk Chat

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+const port = 3000;
+
+io.on('connection', (socket) => {
+  console.log('New client connected');
+  socket.on('chat message', (msg) => {
+    console.log('Nasabah => ' + msg);
+    io.emit('chat message', msg);
+  });
+
+  socket.on('chat message ps', (msg) => {
+    console.log('Pengurus Satu => ' + msg);
+    io.emit('chat message ps', msg);
+  });
+
+  socket.on('chat message pd', (msg) => {
+    console.log('Pengurus Dua => ' + msg);
+    io.emit('chat message pd', msg);
+  });
+});
+
+server.listen(port, () => { console.log('Server listening on port ' + port); })
